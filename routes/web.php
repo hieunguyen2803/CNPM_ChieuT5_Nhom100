10e71490976d2c0b728847dc31a33a0844087089
hieunguyen2803 <55368798+hieunguyen2803@users.noreply.github.com>
2020-08-06 01:21:34 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get( '/', function () {
-	return view( 'welcome' );
+	return view( 'page.home' );
 } );
 Route::get( "home", function () {
 	return view( "page.home" );
